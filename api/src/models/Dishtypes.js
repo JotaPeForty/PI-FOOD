5981810a89d9c3e842bc6436f8ddad46a4871e18
Juan Pablo Benavente <juanp.benavente@gmail.com>
2022-11-09 00:00:45 -0300

    .

--- api/src/models/Dishtypes.js
+++ api/src/models/Dishtypes.js
@@ -1,16 +1,16 @@
-const { DataTypes } = require("sequelize");
+// const { DataTypes } = require("sequelize");
 
-module.exports = (sequelize) => {
-  sequelize.define("dishtypes",
-    {
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      // name:{
-      //     type: DataTypes.ARRAY(DataTypes.STRING),
-      // }
-    },
-    { timestamps: false }
-  );
-};
+// module.exports = (sequelize) => {
+//   sequelize.define("dishtypes",
+//     {
+//       name: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//       },
+//       // name:{
+//       //     type: DataTypes.ARRAY(DataTypes.STRING),
+//       // }
+//     },
+//     { timestamps: false }
+//   );
+// };
--- api/src/models/Steps.js
+++ api/src/models/Steps.js
@@ -1,16 +1,16 @@
-const { DataTypes } = require("sequelize");
+// const { DataTypes } = require("sequelize");
 
-module.exports = (sequelize) => {
-  sequelize.define("steps",
-    {
-      step: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      // name:{
-      //     type: DataTypes.ARRAY(DataTypes.STRING),
-      // }
-    },
-    { timestamps: false }
-  );
-};
+// module.exports = (sequelize) => {
+//   sequelize.define("steps",
+//     {
+//       step: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//       },
+//       // name:{
+//       //     type: DataTypes.ARRAY(DataTypes.STRING),
+//       // }
+//     },
+//     { timestamps: false }
+//   );
+// };
